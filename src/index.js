c2afa61bd5678a4249e5fdbe3b8617cb4704a643
FrankKai <gaokai20100801@gmail.com>
2022-07-05 15:49:46 +0800

    feat: support webpack4

--- src/index.js
+++ src/index.js
@@ -11,30 +11,10 @@ class CopyRenameWebpackPlugin {
     const pluginName = CopyRenameWebpackPlugin.name;
 
     const { webpack } = compiler;
-    // webpack 4
-    if (!webpack?.Compilation) {
-      compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
-        compilation.hooks.afterOptimizeAssets.tap((assets) => {
-          const { entry, output } = this.options;
-          // assets是包含所有静态资源的对象
-          // 在编译阶段，对象的键是assets的路径，对象的值是assets的文件内容
 
-          // 遍历所有静态文件，匹配到需要复制的文件
-          let fileContent = null;
-          for (const [key, value] of Object.entries(assets)) {
-            if (entry === key) {
-              fileContent = value;
-              break;
-            }
-          }
-          // 把资源添加给compilation，从而自动由webpack生成到目标目录
+    const { entry, output } = this.options;
 
-          for (const dir of output) {
-            compilation.emitAsset(dir, fileContent);
-          }
-        });
-      });
-    }
+    let fileContent = null;
 
     // webpack 5
     if (webpack?.Compilation) {
@@ -48,12 +28,10 @@ class CopyRenameWebpackPlugin {
             stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
           },
           (assets) => {
-            const { entry, output } = this.options;
             // assets是包含所有静态资源的对象
             // 在编译阶段，对象的键是assets的路径，对象的值是assets的文件内容
 
             // 遍历所有静态文件，匹配到需要复制的文件
-            let fileContent = null;
             for (const [key, value] of Object.entries(assets)) {
               if (entry === key) {
                 fileContent = value;
@@ -65,6 +43,33 @@ class CopyRenameWebpackPlugin {
             for (const dir of output) {
               compilation.emitAsset(dir, fileContent);
             }
+          }
+        );
+      });
+    }
+    // webpack 4
+    if (!webpack?.Compilation) {
+      compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+        compilation.hooks.optimizeAssets.tapAsync(
+          pluginName,
+          (assets, callback) => {
+            // assets是包含所有静态资源的对象
+            // 在编译阶段，对象的键是assets的路径，对象的值是assets的文件内容
+
+            // 遍历所有静态文件，匹配到需要复制的文件
+            for (const [key, value] of Object.entries(assets)) {
+              if (entry === key) {
+                fileContent = value;
+                break;
+              }
+            }
+            // 把资源添加给compilation，从而自动由webpack生成到目标目录
+
+            for (const dir of output) {
+              // compilation.assets[dir] = fileContent;
+              compilation.emitAsset(dir, fileContent);
+            }
+            callback();
           }
         );
       });
